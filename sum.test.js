2ca374446613dc0dfe0ae694fad655fb4c52854f
Rchod123 <rajesh.chodavarapu3@gmail.com>
2026-06-12 15:25:28 +0530

    changes in the test

--- sum.test.js
+++ sum.test.js
@@ -6,3 +6,6 @@ test('add 1+2 equals 3', () => {
 test('add 1+2 not equals 4', () => {
     expect(sum(1,2)).not.toBe(4);
 });
+test('add 0+2 not equals 1', () => {
+    expect(sum(0,2)).not.toBe(1);
+});
